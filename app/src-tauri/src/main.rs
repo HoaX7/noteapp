0ec3f5bc28a01b358e947df4faddbd0df664bdac
hoax <kvnrivers@gmail.com>
2024-09-28 17:53:50 +0530

    feat: mac support for m1 & intel - added guide file for mac and windows

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -2,18 +2,22 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 mod tray;
+use std::io::{Read, Write};
+
+use noteapp_lib::app_settings;
 use tray::*;
 mod commands;
 mod shortcuts;
 use commands::*;
 mod controller;
-use tauri::Manager;
+use tauri::{AppHandle, Manager};
 use window_shadows::set_shadow;
 
 fn main() {
     tauri::Builder::default()
         .system_tray(make_tray())
         .setup(|app| {
+            let handle = app.handle();
             #[cfg(target_os = "macos")]
             app.set_activation_policy(tauri::ActivationPolicy::Accessory);
 
@@ -28,6 +32,9 @@ fn main() {
                     }
                     _ => {}
                 });
+
+                // add guide to the default path
+                save_guide(&handle);
             }
 
             set_shadow(&window, true).expect("window shadow unsupported for this platform");
@@ -48,4 +55,23 @@ fn main() {
         ])
         .run(tauri::generate_context!())
         .expect("error running application");
+}
+
+fn save_guide(handle: &AppHandle) {
+    let resource_path = handle.path_resolver()
+    .resolve_resource("./docs/guide_DO_NOT_EDIT.md")
+    .expect("failed to resolve resource");
+    let mut file = std::fs::File::open(&resource_path).unwrap();
+    let contents = &mut String::default();
+    file.read_to_string(contents).unwrap();
+    let path = app_settings::make_path("guide_DO_NOT_EDIT.md");
+    let mut new_file = std::fs::OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open(path)
+        .unwrap();
+    new_file.write_all(contents.as_bytes()).unwrap();
+    new_file.flush().unwrap();
+    file.flush().unwrap();
 }
